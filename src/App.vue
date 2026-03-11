f818c62c3ec895981dbe311a5b8d52a932f08711
Jan <jan.lahse@gmx.de>
2026-03-11 16:11:33 +0100

    added more snippet cards

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 import { ref } from 'vue'
 import SnippetCard from './components/SnippetCard.vue'
 
-const dummySnippet = ref({
-  id: 1,
-  title: 'HTML Boilerplate',
-  tag: 'HTML',
-  content: `<!DOCTYPE html>
+const snippetsList = ref([
+  {
+    id: 1,
+    title: 'HTML Boilerplate',
+    tag: 'HTML',
+    isActive: true,
+    content: `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
@@ -19,13 +21,59 @@ const dummySnippet = ref({
   <h1>Hello, world!</h1>
 </body>
 </html>`,
-})
+  },
+  {
+    id: 2,
+    title: 'CSS Reset',
+    tag: 'CSS',
+    isActive: false,
+    content: `*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}`,
+  },
+  {
+    id: 3,
+    title: 'CSS Reset',
+    tag: 'CSS',
+    isActive: true,
+    content: `*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}`,
+  },
+  {
+    id: 4,
+    title: 'HTML Boilerplate',
+    tag: 'HTML',
+    isActive: false,
+    content: `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Hello, world!</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <meta name="description" content="" />
+  <link rel="icon" href="favicon.png">
+</head>
+<body>
+  <h1>Hello, world!</h1>
+</body>
+</html>`,
+  },
+])
 </script>
 
 <template>
   <main>
     <h1>Code Snippet Manager</h1>
-    <SnippetCard :snippet="dummySnippet" />
+    <section class="displayedSnippets">
+      <SnippetCard v-for="snippet in snippetsList" :key="snippet.id" :snippet="snippet" />
+    </section>
   </main>
 </template>
 
@@ -51,5 +99,11 @@ main {
   > * {
     margin-bottom: 30px;
   }
+}
+
+.displayedSnippets {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 30px;
 }
 </style>
--- src/components/SnippetCard.vue
+++ src/components/SnippetCard.vue
@@ -3,10 +3,12 @@ const props = defineProps(['snippet'])
 </script>
 
 <template>
-  <section>
+  <section :class="props.snippet.isActive && 'snippet-active'">
     <h2>{{ props.snippet.title }}</h2>
-    <span>HTML</span>
-    <pre>{{ props.snippet.content }}</pre>
+    <span>{{ props.snippet.tag }}</span>
+    <div>
+      <pre>{{ props.snippet.content }}</pre>
+    </div>
   </section>
 </template>
 
@@ -14,13 +16,20 @@ const props = defineProps(['snippet'])
 @use '/src/base';
 
 section {
+  max-width: 45%;
+  height: 190px;
   display: inline-flex;
   flex-direction: column;
-  gap: 20px;
+  gap: 10px;
   align-items: start;
   border: 2px solid base.$primary-color;
   border-radius: 10px;
   padding: 20px 30px;
+}
+
+.snippet-active {
+  height: unset;
+  max-width: unset;
 }
 
 span {
@@ -29,5 +38,19 @@ span {
   border-radius: 5px;
   color: base.$white;
   font-weight: bold;
+}
+
+div {
+  border-radius: 10px;
+  padding: 4px;
+  overflow: hidden;
+  width: 100%;
+  color: base.$primary-color-soft;
+  background-color: white;
+}
+
+pre {
+  font-family: Courier, monospace;
+  font-size: 0.9em;
 }
 </style>
